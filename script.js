c448f92382e188ae06a33a7808ed19f8acf302fb
RolandHU <85389397+RolandHU@users.noreply.github.com>
2021-12-02 17:00:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ audio.addEventListener("timeupdate", (e) => {
     console.log(e.target.currentTime)
 })
 
-const asd = fetch("https://rolandhu.github.io/song_player/playlist/songList.json").then (response => response.json()).then (data => data)
-console.log(asd)
+const songList = fetch("https://rolandhu.github.io/song_player/playlist/songList.json").then (response => response.json()).then (data => data)
+audio.setAttributes("src", songList[0].songSrc)
