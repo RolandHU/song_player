970e347212d952132bb5e0e697747ac9a7891f07
RolandHU <85389397+RolandHU@users.noreply.github.com>
2021-12-05 16:51:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loadLyrics()
 document.addEventListener("visibilitychange", () => {
     if (document.visibilityState == "hidden") {
         audio.pause()
-    } else if (document.visibilityState == "visible") {
+    } else if (document.visibilityState == "visible" && audio.currentTime > 0) {
         audio.play()
     }
 })
@@ -38,12 +38,24 @@ async function loadLyrics() {
     }
 }
 
+var dropList
+var latest = 0
 var lastSec = 0
 async function getSongs(sec) {
     const songs = await getData("https://rolandhu.github.io/song_player/playlist/songList.json")
     const lyrics = await getData(songs[0].lyricsSrc)
     const lines = document.querySelectorAll(".line")
-    
+
+    if (dropList.find(elem => elem == sec)) {
+        if (latest != sec) {
+            document.body.style.filter = "invert(1)"
+            setTimeout(() => {
+                document.body.style.filter = "invert(0)"
+            }, 100)
+            latest = sec
+        }
+    }
+
     var pos = 0
     for (let i = 0; i < lyrics.length; i++) {
         if (sec == lyrics[i].second && sec != lastSec) {
@@ -57,6 +69,7 @@ async function getSongs(sec) {
                     elem.style.color = "rgba(255, 255, 255, 0.3)"
                 }
             })
+
             lastSec = sec
             pos += 1
         }
@@ -68,4 +81,10 @@ async function getAudio() {
     audio.setAttribute("src", songs[0].songSrc)
 }
 
+async function getDrops() {
+    const songs = await getData("https://rolandhu.github.io/song_player/playlist/songList.json")
+    dropList = await getData(songs[0].dropSrc)
+}
+
 getAudio()
+getDrops()
